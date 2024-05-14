0f975398aa3409b67cff8c46230a4daa9d663ef0
makulimike <143262975+makulimike@users.noreply.github.com>
2024-05-14 11:31:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function showmenulist(){
+    const showmenulist = document.querySelector('.sidebarmenus')
+    showmenulist.style.display = 'flex'
+}
+function closemenulist(){
+    const closemenulist = document.querySelector('.sidebarmenus')
+    closemenulist.style.display = 'none'
+}
